4997550539d2f4f4b95ba1d24f7ebeb306942bda
BettinaP <bettinaprophete@gmail.com>
2016-06-18 17:30:35 -0400

    done

--- OOP-Cards-Model/FISAppDelegate.m
+++ OOP-Cards-Model/FISAppDelegate.m
@@ -1,11 +1,20 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISCardDeck.h"
+#import "FISCard.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISCard *defaultCard =[[FISCard alloc]init];
+    NSLog(@"%@",defaultCard);
+    
+    
+    FISCardDeck *defaultDeck =[[FISCardDeck alloc]init];
+    NSLog(@"############%@", defaultDeck.description);
     return YES;
 }
 
